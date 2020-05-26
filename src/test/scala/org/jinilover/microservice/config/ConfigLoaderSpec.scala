b298b1f9d425e9015b06f8d1ef2cefc2b35f4742
jinilover <columbawong@gmail.com>
2020-05-26 20:48:10 +1000

    Add Config test

---
+++ src/test/scala/org/jinilover/microservice/config/ConfigLoaderSpec.scala
@@ -0,0 +1,27 @@
+package org.jinilover.microservice.config
+
+import org.jinilover.microservice.ConfigTypes.{AppConfig, DbConfig, WebServerConfig}
+import org.specs2.Specification
+import org.specs2.specification.core.SpecStructure
+
+class ConfigLoaderSpec extends Specification {
+  override def is: SpecStructure =
+    s2"""
+        ConfigLoader
+          should load the required AppConfig $loadAppConfig
+      """
+
+  def loadAppConfig = {
+    val expected = AppConfig(
+      DbConfig(
+        "jdbc:postgresql://localhost:5432/postgres"
+      , "postgres"
+      , "password")
+    , WebServerConfig(
+        "0.0.0.0"
+      , 8080)
+    )
+
+    ConfigLoader.default.load.unsafeRunSync() must be_==(expected)
+  }
+}
